8825271135d3c5b35326e0cbbf8fd9fdd162d62d
Thomas Oo <thein.oo@mail.mcgill.ca>
2018-10-11 13:29:50 -0400

    init

---
+++ MDK-ARM/Lab3_Uart/startup_stm32l475xx.d
@@ -0,0 +1,1 @@
+lab3_uart\startup_stm32l475xx.o: startup_stm32l475xx.s
---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Lab3_Uart' 
+ * Target:  'Lab3_Uart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
